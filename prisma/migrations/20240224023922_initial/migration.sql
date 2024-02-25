d22aa80ad177b65d083bc0dbef441a1b66753619
SAAD <saadsyed950@gmail.com.com>
2024-02-25 09:32:31 +0530

    Rockstarrrr

--- prisma/migrations/20240224023922_initial/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Lunch" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "price" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Lunch_name_key" ON "Lunch"("name");
