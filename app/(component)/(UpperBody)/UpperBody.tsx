e23f62306f0a96946aaa6a938e2079e488b94589
SAAD <saadsyed950@gmail.com.com>
2024-02-28 00:05:12 +0530

    Swimming Pools

--- app/(component)/(UpperBody)/UpperBody.tsx
+++ app/(component)/(UpperBody)/UpperBody.tsx
@@ -15,12 +15,9 @@ const UpperBody = () => {
   const [banquetBut, setBanquetBut] = useState(false);
   return (
     <>
-      <Link
-        href="/"
-        className="text-center tracking-wider lg:hidden text-[3.5rem] uppercase font-bold mt-32 font-unbounded"
-      >
+      <h1 className="text-center tracking-wider lg:hidden text-[3.5rem] uppercase font-bold mt-32 font-unbounded">
         Grove
-      </Link>
+      </h1>
       <div className="flex justify-center items-center flex-row mt-8">
         <Button
           style="border-[1px] border-black lg:px-6 lg:py-3 px-2 py-1 tracking-wide lg:tracking-wider lg:ml-6 ml-2  hover:bg-black hover:text-gray-200 duration-300 hover:rounded-lg"
