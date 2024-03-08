676a744c625584039b9dcb980983ce3c5da305cb
SAAD <saadsyed950@gmail.com.com>
2024-03-08 18:21:17 +0530

    Fa

--- app/(component)/(SecNavbar)/SecNavbar.tsx
+++ app/(component)/(SecNavbar)/SecNavbar.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Menu from "./Menu";
 import { RiMenuFoldLine } from "react-icons/ri";
+import { BiX } from "react-icons/bi";
 import Link from "next/link";
 
 const SecNavbar = () => {
@@ -15,7 +16,10 @@ const SecNavbar = () => {
   return (
     <>
       <div className="fixed flex border-b-2 shadow-[0.1rem_0.25rem_10px_0px_rgba(0,0,0,1)] border-black top-0 w-full bg-[#e7e7e7] lg:hidden md:hidden sm:flex justify-between items-center">
-        <Link href="/" className="p-5 lg:text-4xl sm:text-3xl md:text-3xl text-2xl uppercase font-josefin font-semibold">
+        <Link
+          href="/"
+          className="p-5 lg:text-4xl sm:text-3xl md:text-3xl text-2xl uppercase font-josefin font-semibold"
+        >
           Bonjour
         </Link>
         <nav className="flex flex-row">
@@ -23,7 +27,7 @@ const SecNavbar = () => {
             className="px-6 py-5 lg:mr-8 mr-2 cursor-pointer text-2xl"
             onClick={menuFunc}
           >
-            <RiMenuFoldLine />
+            {menuOpen ? <BiX /> : <RiMenuFoldLine />}
           </h1>
         </nav>
       </div>
